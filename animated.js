f6fc35c79c17e59d55c8b704a23e78599720101d
Muhammad Ridwan <ridhoang93@gmail.com>
2021-07-30 03:22:06 +0800

    UPDATE (Rebuild tampilan products, Menambahkan animasi ketikan di bagian header products,  Membuat script JS manual, Memanggil CDN typed.js untuk include animasi)

---
+++ animated.js
@@ -0,0 +1,7 @@
+//memanggil function Typed dari dari CDN typed.js
+new Typed('#typed', {
+    strings : [`Let's Make your desktop tobe colorfull`], //string yang akan di tampilkan ke inisial id #typed html
+    typeSpeed : 100, //kecepatan animasi
+    delaySpeed : 200, //delay animasi 
+    loop : true //verifikasi looping untuk animasi :)
+});
